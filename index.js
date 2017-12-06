c297a3973bb986dda1da24f4f8431ee1377f155d
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-12-06 12:20:27 -0800

    Initial start of Git

---
+++ index.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+    $('.carousel').carousel({
+      interval: 2000
+    });
+
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: false,
+        dots: false,
+          pauseOnHover: false,
+          responsive: [{
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        }, {
+          breakpoint: 520,
+          settings: {
+            slidesToShow: 2
+          }
+        }]
+      });
+    //NAvigate between two navs
+    $(window).scroll(function() {
+        // $('.my-hidden-nav').hide();
+      if ($(this).scrollTop()>650)
+       {
+          $('.my-hidden-nav').show();
+       }
+      else
+       {
+        $('.my-hidden-nav').hide();
+       }
+    });
+
+    //Scroll to id wanted
+    $(".nav-link").click(function(e) {
+    e.preventDefault();
+    var aid = $(this).attr("href");
+    $('html,body').animate({scrollTop: $(aid).offset().top},'slow');
+});
+});
+
+
+
+
